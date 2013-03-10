4ff9378a16bef89a95b95f2278717ea46f091e10
Peter Sugihara <peter@campsh.com>
2013-03-09 23:40:24 -0500

    created spree store

---
+++ store/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '06576f27eaf0c2c06af211eec08edecc53b4f6c42e29f486f95bfbc2e2b730fddd5ae130fc00b2003b54eb0c12fd89ee9c31c68d2b4e6f6ebf5334452524c742'
